571625bd41b0bf258374173b2a44a1f884b12fb0
kunpaoo <txcokun@gmail.com>
2024-03-29 16:46:01 +0800

    Navbar adjustments

--- front-end/react-app/src/App.jsx
+++ front-end/react-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Header></Header>
-      <div className="border border-black w-full h-auto p-4">TAILWIND HEREZ</div>
+      <div className="border border-black w-full h-auto p-4 font-main">TAILWIND HEREZ</div>
 
     </>
   )
