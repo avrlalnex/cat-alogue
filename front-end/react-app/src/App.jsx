ee39bad89cf5cd8e078234ac8d8d041dcf1352cb
kunpaoo <txcokun@gmail.com>
2024-03-29 14:46:22 +0800

    move files to front-end folder

--- front-end/react-app/src/App.jsx
+++ front-end/react-app/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Header from './Header'
 
 function App() {
   
 
   return (
     <>
+      <Header></Header>
       <div className="border border-black w-full h-auto p-4">TAILWIND HEREZ</div>
+
     </>
   )
 }
