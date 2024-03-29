e0dfc3d475a62601406375065d07e2a3dfb70fb6
kunpaoo <txcokun@gmail.com>
2024-03-29 14:31:42 +0800

    added react + vite + tailwind + flowbite base

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="border border-black w-full h-auto p-4">TAILWIND HEREZ</div>
+    </>
+  )
+}
+
+export default App
