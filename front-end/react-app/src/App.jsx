1c8cb6e8009a4587848497169d646d69fd78a1b5
kun <120943486+kunpaoo@users.noreply.github.com>
2024-03-29 14:48:58 +0800

    Update App.jsx

--- front-end/react-app/src/App.jsx
+++ front-end/react-app/src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css'
-import Header from './Header'
 
 function App() {
   
 
   return (
     <>
-      <Header></Header>
       <div className="border border-black w-full h-auto p-4">TAILWIND HEREZ</div>
 
     </>
