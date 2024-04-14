b9ae88f5f0ca8ac1bafac55bed7b21a8e5f426f6
CatSnaxx <aaron.justin.macias@gmail.com>
2024-04-14 20:46:27 +0800

    updated Login functionality

--- front-end/react-app/src/Login/LoginForm.jsx
+++ front-end/react-app/src/Login/LoginForm.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
 import './LoginForm.css';
-import logo from './cat_icon.svg'; 
+import logo from '../assets/cat_icon.svg'; 
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const LoginForm = () => {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+  
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //makes get request to api to check credentials
+    axios.post('http://127.0.0.1:8000/login/', formData)
+    .then(response => {
+      console.log(response.data)
+    }
+  )
+      
+    .catch(error => {console.log(error)})
+  }
+
+
   return (
     <div className="flex justify-center items-center min-h-screen pb-12">
       <div className="flex flex-col items-center">
@@ -10,7 +38,7 @@ const LoginForm = () => {
         <div className="text-lg text-white font-lilita mb-4">cat-alogue</div> 
 
         <div className="bg-white p-8 border rounded-3xl shadow-lg">
-          <form action="">
+          <form action="" onSubmit = {handleSubmit}>
             <div className="flex justify-center mb-6">
               <h1 className="text-2xl text-orange-500 font-lilita text-center">
                 Sign in
@@ -19,13 +47,13 @@ const LoginForm = () => {
 
             <div className="flex justify-center mb-4">
               <div className="w-84">
-                <input type='text'placeholder='Username' className="w-96 px-4 py-2 border rounded-full border-orange-500 focus:outline-none focus:border-orange-600" required/>
+                <input type='text'placeholder='Username' className="w-96 px-4 py-2 border rounded-full border-orange-500 focus:outline-none focus:border-orange-600" name = "username" value = {formData.username} onChange = {handleChange} required/>
               </div>
             </div>
 
             <div className="flex justify-center mb-6">
               <div className="w-84">
-                <input type='password' placeholder='Password' className="w-96 px-4 py-2 border rounded-full border-orange-500 focus:outline-none focus:border-orange-600" required/>
+                <input type='password' placeholder='Password' className="w-96 px-4 py-2 border rounded-full border-orange-500 focus:outline-none focus:border-orange-600" name = "password" value = {formData.password} onChange = {handleChange} required/>
               </div>
             </div>
 
