5a28824dcb5a7b9d07308c985fc9330320a15475
Avril Lalaine <93114301+avrlalnex@users.noreply.github.com>
2024-05-14 10:44:22 +0800

    Update Block.jsx

--- front-end/react-app/src/About/Block.jsx
+++ front-end/react-app/src/About/Block.jsx
@@ -15,7 +15,7 @@ function Block() {
           <p className="mb-8 font-main text-cat-details">
           Cat-alogue is a heartfelt endeavor committed to revolutionizing the way individuals find their furry companions. Our platform stands as a beacon of hope for cats awaiting their forever homes, offering a user-friendly interface that showcases their unique personalities and charms. Through Cat-alogue, we strive to not only facilitate adoptions but also cultivate a community centered around compassion, education, and support for all aspects of cat welfare.
           </p>
-          <p className='text-secondary'>
+          <p className='text-cat-details'>
           In addition to facilitating adoptions, Cat-alogue serves as an educational hub, providing resources and guidance on responsible pet ownership, feline care, and behavior. We believe that every cat deserves a loving home and every adopter deserves the knowledge and tools necessary to provide the best possible care. By empowering both shelters and adopters with the information and support they need, we aim to make every adoption journey with Cat-alogue a rewarding and fulfilling experience for both cats and their human companions.
           </p>
         </div>
